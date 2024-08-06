126fb8310c31e87fec1313342389444bd039aa94
Georgio Mazraani <mazraanigeorgio@gmail.com>
2024-08-06 12:22:47 +0300

    removed the volume setter + added go live button

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ curr_track.src = streamURL;
 curr_track.load();
 
 const playpause_btn = document.querySelector('.playpause-track');
-const volume_slider = document.querySelector('.volume_slider');
 
 curr_track.addEventListener('error', (e) => {
     console.error('Error loading the audio track:', e);
@@ -37,34 +36,14 @@ function pauseTrack() {
     playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
 
-function setVolume() {
-    curr_track.volume = volume_slider.value / 100;
-    console.log('Volume set to:', curr_track.volume);
+
+function goLive() {
+    curr_track.currentTime = curr_track.buffered.end(curr_track.buffered.length - 1);
+    curr_track.play().then(() => {
+        isPlaying = true;
+        playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+    }).catch(error => {
+        console.error('Error going live:', error);
+        alert('Error going live. Please try again.');
+    });
 }
-
-// Update volume slider to match the current volume
-function updateVolumeSlider() {
-    volume_slider.value = curr_track.volume * 100;
-    console.log('Volume slider updated to:', volume_slider.value);
-}
-
-// Detect visibility change to update volume
-document.addEventListener('visibilitychange', () => {
-    if (!document.hidden) {
-        // Update the volume to match the device volume when the page becomes visible
-        curr_track.volume = volume_slider.value / 100;
-        console.log('Visibility changed. Volume updated to:', curr_track.volume);
-    }
-});
-
-// Event listener for volume slider
-volume_slider.addEventListener('input', () => {
-    setVolume();
-    console.log('Volume slider input detected. Volume set to:', volume_slider.value);
-});
-
-// Add touch event listener for mobile devices
-volume_slider.addEventListener('touchend', () => {
-    setVolume();
-    console.log('Volume slider touchend detected. Volume set to:', volume_slider.value);
-});
